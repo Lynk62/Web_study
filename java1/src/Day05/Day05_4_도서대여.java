beddc2419ab117d4e1904bb6582a04e19224ff28
504 <504@DESKTOP-BUQ6K3C>
2022-03-07 17:26:57 +0900

    Day05_배열

--- java1/src/Day05/Day05_4_도서대여.java
+++ java1/src/Day05/Day05_4_도서대여.java
@@ -11,6 +11,9 @@ public class Day05_4_도서대여 {//cs
 		String[][]memberlist =	new String[100][2];
 		String[][]booklist = new String[100][3];
 		
+		String 가능 = "대여가능";
+		String 불가능 = "대여중";
+		
 		while(true) {
 			
 			System.out.println("-------[[ 회원제 도서대여 ]]------");
@@ -53,13 +56,15 @@ public class Day05_4_도서대여 {//cs
 				
 				for(int i=0; i<memberlist.length; i++) {
 					
+					if(logincheck==true) break;
+					
 					if(id.equals("admin")) {
 						System.out.println("관리자 로그인");
 						logincheck = true;
 						
 						while(true) {
 							System.out.println("[[ 관리자 메뉴 ]]");
-							System.out.println("1.도서 등록    2.도서 목록    3.로그아웃\n>>선택 : ");	int ch3 = scan.nextInt();
+							System.out.println("1.도서 등록     2.도서 목록     3.도서 삭제    4.로그아웃\n>>선택 : ");	int ch3 = scan.nextInt();
 							
 							if(ch3==1) {
 								System.out.println("도서명 : ");		String title = scan.next();
@@ -68,6 +73,8 @@ public class Day05_4_도서대여 {//cs
 								for(int j=0; j<booklist.length; j++) {
 									if(booklist[j][0] == null) {
 										booklist[j][0] = title;
+										booklist[j][1] = 가능;
+										System.out.println("도서를 등록하였습니다");
 										break;
 									}//if end
 									
@@ -92,8 +99,14 @@ public class Day05_4_도서대여 {//cs
 							}//2.도서목록 if end
 							
 							else if(ch3==3) {
+								
+								
+							}//3.도서삭제 if end
+							
+							else if(ch3==4) {
+								System.out.println("로그아웃 되었습니다");
 								break;
-							}//3.로그아웃 if end
+							}//4. 로그아웃 if end
 							
 						}//관리자 while end
 					}//admin if end
@@ -106,26 +119,63 @@ public class Day05_4_도서대여 {//cs
 						
 						while(true) {
 							System.out.println("------[[ 도서 메뉴 ]]-------");
-							System.out.println("1.도서 검색    2.도서 목록    3.도서 대여    4.도서 반납\n>>선택 : ");	int ch2 = scan.nextInt();
+							System.out.println("1.도서 검색    2.도서 목록    3.도서 대여    4.도서 반납     5.로그아웃\n>>선택 : ");	int ch2 = scan.nextInt();
 							
 							if(ch2==1) {	//도서 검색
 								System.out.println("도서명 : ");		String bookname = scan.next();
 								
-								
 								for(int j=0; j<booklist.length; j++) {
+									if(booklist[j][0] != null && booklist[j][0].equals(bookname)) {
+										System.err.println("도서명\t도서대여여부\t대여자");
+										System.out.println("----------------------");
+										System.out.printf("%s\t%s\t%s\n\n", booklist[j][0],booklist[j][1],booklist[j][2]);
+										break;
+									}//if end
 									
+									else {
+										System.out.println("도서가 없습니다");
+										break;
+									}//도서없음 else end
 								}//for end
 							}//ch2==1 if end
+							
 							
 							else if(ch2==2) {
 								System.out.println("------[[ 도서 목록 ]]-------");
 								System.out.println("번호\t도서명\t대여여부\t대여자");
 								
+								for(int j=0; j<booklist.length; j++) {
+									System.out.printf("%d\t%s\t%s\t%s\n\n", j,booklist[j][0],booklist[j][1],booklist[j][2]);
+									break;
+								}//for end
 							}//ch2==2 if end
 							
-							else if(ch2==3) {}//ch2==3 if end
+							else if(ch2==3) {
+								
+								System.out.println("대여할 도서명 : "); 	String bookname2 = scan.next();
+								
+								for(int j=0; j<booklist.length; j++) {
+									if(booklist[j][0].equals(bookname2)) {
+										if(booklist[j][1].equals(가능)) {
+											System.out.printf("%s를 대여하였습니다\n", booklist[j][0]);
+											booklist[j][1] = 불가능;
+											booklist[j][2] = memberlist[j][0];
+											break;
+										}//대여가능여부 if end
+										
+										else {}//
+									}//같은도서명 찾기 if end
+								}//도서대여 for end
+								
+							}//ch2==3 if end
 							
 							else if(ch2==4) {}//ch2==4 if end
+							
+							else if(ch2==5) {
+								break;
+							}//ch2==4 if end
+							
+							else {}//잘못된입력 else end
 							
 					
 						}//로그인 후 무한루프 while end	
