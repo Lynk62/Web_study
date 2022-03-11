631524b8f03051cdc3d7cd9cfba4e164c2d0dae4
504 <504@DESKTOP-BUQ6K3C>
2022-03-11 14:50:08 +0900

    Day08_메소드2

--- java1/src/Day08/Day08_5_모바일뱅크.java
+++ java1/src/Day08/Day08_5_모바일뱅크.java
@@ -28,59 +28,34 @@ public class Day08_5_모바일뱅크 {//cs
 	
 	//초기메뉴
 	void menu() {
-		
-		while(true) {
-			System.out.println("--------------------- 도서 대여 프로그램 ---------------------");
-			System.out.println("1.회원가입    2.로그인    3.아이디찾기    4.비밀번호찾기 \n 선택 : ");
-			int ch = scanner.nextInt();
-			
-			Member2 member = new Member2();//1~4 {}에서 사용하기 위해 
-			
-			if(ch==1) {
-				boolean result = member.회원가입();//1.회원가입 메소드 호출
-				if(result == true) {
-					System.out.println("[ 알림 ] 회원가입 성공!");
-				}//true if end
+		Member2 member = new Member2();
+			while(true) {
+				System.out.println("----------------------------------------------");
+				System.out.println("1. 회원가입 2 . 로그인 3. 아이디찾기 4. 비밀번호찾기 :");
+				int ch = scanner.nextInt();		
+
+				if(ch==1) {
+					boolean result = member.회원가입();// 회원가입 메소드 호출
+					if(result) System.err.println("회원가입 성공");
+					else System.err.println("회원가입 실패");
+				}
 				
-				else System.out.println("[ 알림 ] 회원가입 실패");//false else end
-			}//1.회원가입 if end
-			
-	//----------------------------------------------------------------------------------		
-			
-			else if(ch==2) {
-				String result = member.로그인();//2.로그인 메소드 호출
-				
-				//1. 일반회원 메뉴 메소드 호출
-				if(result == null) {
-					System.out.println("알림)) 로그인 실패! 회원정보가 없습니다");
-				}//로그인실행 if end
-				
-				else {
-					//1.일반회원 메뉴 메소드 호출
-					System.out.println("알림)) 안녕하세요!" + result + "님");
-					bankmenu(result);	//인수1개 [로그인성공한 id] 전달
-				}//일반메뉴 호출 else end
-				
-			}//2.로그인 if end
-			
-	//----------------------------------------------------------------------------------		
-			
-			else if(ch==3) {
-				member.아이디찾기();//3.아이디찾기 메소드 호출
-				
-			}//3.아이디찾기 if end
-			
-			else if(ch==4) {
-				member.비밀번호찾기();//4.비밀번호찾기 메소드 호출
-				
-			}//4.비밀번호찾기 if end
-			
-			else {
-				System.out.println("알림)) 알 수 없는 번호입니다");
-			}//else end
-			
-		}//무한 반복 while end
-	}//void end
+				else if(ch==2) {// 로그인 메소드 호출
+					String result = member.로그인();
+					if(result == null) {
+						System.out.println("동일한 회원정보가 없습니다.");
+					}
+					else {
+						System.out.println("안녕하세요."+result+"님"); 
+					}
+				}
+				else if(ch==3) {member.아이디찾기();}
+					// 아이디 찾기 메소드 호출	
+				else if(ch==4) {member.비밀번호찾기();}
+					// 비밀번호 찾기 메소드 호출
+				else {System.err.println("알림) 알수 없는 번호입니다.");}
+		}
+	}//메뉴 void end
 	
 	
 	//은행메뉴
@@ -104,18 +79,7 @@ public class Day08_5_모바일뱅크 {//cs
 	//----------------------------------------------------------------------------------		
 			
 			else if(ch1==2) {
-//				String result = bank.입금();//2.입금 메소드
-//				//1. 일반회원 메뉴 메소드 호출
-//				if(result == null) {
-//					System.out.println("알림)) 이체가 완료되었습니다");
-//				}//로그인실행 if end
-//				
-//				else {
-//					//1.일반회원 메뉴 메소드 호출
-//					System.out.println("알림)) 안녕하세요!" + result + "님");
-//					bankmenu(result);	//인수1개 [로그인성공한 id] 전달
-				}//일반메뉴 호출 else end
-				
+				bank.입금();//2.입금 메소드
 			}//2.입금 if end
 			
 	//----------------------------------------------------------------------------------		
@@ -138,6 +102,6 @@ public class Day08_5_모바일뱅크 {//cs
 	//1.계좌생성
 	//2.입금
 	//출금 / 4.이체 / 5.대출 / 6.계좌목록
-//	}//은행메뉴 void end
+	}//은행메뉴 void end
 	
 }//ce
